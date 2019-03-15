112e3de85cb18d612bfb8d592aa340e440056c28
oksana_kulish <asterisk.hu@gmail.com>
2019-03-15 23:14:51 +0200

    Add project files.

---
+++ CypherHilla/HillCipher.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace CypherHilla
+{
+    public class HillCipher 
+    {
+        public static int num = 26;
+        public int Det(Matrix<double> M)
+        {
+            double A = M[0, 0] * (M[1, 1] * M[2, 2] - M[1, 2] * M[2, 1]) -
+                       M[0, 1] * (M[1, 0] * M[2, 2] - M[1, 2] * M[2, 0]) +
+                       M[0, 2] * (M[1, 0] * M[2, 1] - M[1, 1] * M[2, 0]);
+            int AI = (int)A % num >= 0 ? (int)A % num : (int)A % num + num;
+            for (int i = 0; i < num; i++)
+            {
+                if (AI * i % num == 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public Matrix<double> ModMinorCofactor(Matrix<double> M, int A)
+        {
+            Matrix<double> resMat = DenseMatrix.Create(3, 3, 0.0);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int x = i == 0 ? 1 : 0, y = j == 0 ? 1 : 0, x1 = i == 2 ? 1 : 2, y1 = j == 2 ? 1 : 2;
+                    double r = ((M[x, y] * M[x1, y1] - M[x, y1] * M[x1, y]) * Math.Pow(-1, i + j) * A) % num;
+                    resMat[i, j] = r >= 0 ? r : r + num;
+                }
+            }
+            return resMat;
+        }
+        
+        public List<int> Decrypt(List<int> cipherText, List<int> key)
+        {
+            List<double> keyD = key.ConvertAll(x => (double)x);
+            List<double> CD = cipherText.ConvertAll(x => (double)x);
+            int m = Convert.ToInt32(Math.Sqrt((key.Count)));
+            Matrix<double> keyMatrix = DenseMatrix.OfColumnMajor(m, (int)key.Count / m, keyD.AsEnumerable());
+            Console.WriteLine(keyMatrix);            
+            
+             Matrix<double> PMatrix = DenseMatrix.OfColumnMajor(m, (int)cipherText.Count / m, CD.AsEnumerable());
+            Console.WriteLine(PMatrix);
+            List<int> finalRes = new List<int>();
+            if (keyMatrix.ColumnCount == 3)
+            {
+                keyMatrix = ModMinorCofactor(keyMatrix.Transpose(), Det(keyMatrix));
+            }            
+            else
+            {
+                keyMatrix = keyMatrix.Inverse();
+                Console.WriteLine(keyMatrix.ToString());
+                Console.WriteLine(((int)keyMatrix[0, 0]).ToString() + ", " + ((int)keyMatrix[0, 0]).ToString());
+            }
+            
+            if (Math.Abs((int)keyMatrix[0, 0]).ToString() != Math.Abs((double)keyMatrix[0, 0]).ToString())
+            {
+                throw new SystemException();
+            }
+            for (int i = 0; i < PMatrix.ColumnCount; i++)
+            {
+                List<double> Res = new List<double>();
+                Res = ((((PMatrix.Column(i)).ToRowMatrix() * keyMatrix) % num).Enumerate().ToList());
+                for (int j = 0; j < Res.Count; j++)
+                {
+                    int x = (int)Res[j] >= 0 ? (int)Res[j] : (int)Res[j] + num;
+                    finalRes.Add(x);
+                }
+            }
+
+            /*for (int i = 0; i < finalRes.Count; i++)
+            {
+                Console.WriteLine(finalRes[i].ToString());
+            }*/
+            return finalRes;
+        }
+        public void Encrypt_Mock(List<int> plainText_Mock, List<int> key_mock)
+        {
+            List<double> keyD = key_mock.ConvertAll(x => (double)x);
+            if (keyD.Count % num != 0)
+            {
+                while(keyD.Count % num != 0)
+                    keyD.Add(0);
+            }
+            List<double> PD = plainText_Mock.ConvertAll(x => (double)x);
+            if (PD.Count % num != 0)
+            {
+                while (PD.Count % num != 0)
+                    PD.Add(0);
+            }
+            int m = Convert.ToInt32(Math.Sqrt((3)));
+            /*Matrix<double> keyMatrix = DenseMatrix.OfColumnMajor(3, (int)keyD.Count / m, keyD.AsEnumerable());
+             Console.WriteLine(keyMatrix);
+
+             Matrix<double> PMatrix = DenseMatrix.OfColumnMajor(3, (int)PD.Count / m, PD.AsEnumerable());
+             Console.WriteLine(PMatrix);
+             for (int i = 0; i < 3; i++)
+             {
+                 for (int j = 0; j < 2; j++)
+                     Console.Write(j);
+                 Console.WriteLine(i);
+              } 
+             foreach (int i in plainText_Mock)
+                 Console.Write(i);*/
+        }
+
+        public List<int> Encrypt(List<int> plainText, List<int> key)
+        {
+            List<double> keyD = key.ConvertAll(x => (double)x);
+         
+            List<double> PD = plainText.ConvertAll(x => (double)x);
+            int m = Convert.ToInt32(Math.Sqrt((key.Count)));
+            Matrix<double> keyMatrix = DenseMatrix.OfColumnMajor(m, (int)key.Count / m, keyD.AsEnumerable());            
+            //Console.WriteLine(keyMatrix);
+            
+            Matrix<double> PMatrix = DenseMatrix.OfColumnMajor(m, (int)plainText.Count / m, PD.AsEnumerable());
+            //Console.WriteLine(PMatrix);
+            
+            List<int> finalRes = new List<int>();
+            for (int i = 0; i < PMatrix.ColumnCount; i++)
+            {
+                List<double> Res = new List<double>();
+                Res = ((((PMatrix.Column(i)).ToRowMatrix() * keyMatrix) % num).Enumerate().ToList());
+               
+                for (int j = 0; j < Res.Count; j++)
+                {
+                    finalRes.Add((int)Res[j]);
+                }
+            }
+            return finalRes;
+        }
+    }
+}
---
+++ CypherHilla/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CypherHilla
+{
+
+    class Program
+    {
+
+        #region AlphavitMock
+        static readonly string abcEng = "abcdefghijklmnopqrstuvwxyz";
+        #endregion
+        static readonly string abcAll= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЄЖЗИІЇйКЛМНОПРСТУФХЦЧШЩЬЮЯ1234567890 ?\"'";
+        
+        static void Main(string[] args)
+        {              
+
+            HillCipher hillCipher = new HillCipher();
+            List<int> key = new List<int>();
+            List<int> key_mock = new List<int>();
+            List<int> plainText = new List<int>();
+            List<int> plainText_Mock = new List<int>();
+            List<int> cipher = new List<int>();
+            string encode_rezult = "";
+            string decode_rezult = "";
+
+            #region Alphabet
+            Console.WriteLine("Alphabet");
+            Dictionary<int, char> alphabet = new Dictionary<int, char>();
+            for (int i = 0; i < abcEng.Length; i++)
+                alphabet.Add(i, abcEng[i]);
+            #endregion
+
+            #region AlphbetMock
+            Dictionary<int, char> alphabet_Mock = new Dictionary<int, char>();
+            for (int i = 0; i < abcAll.Length; i++)
+                alphabet_Mock.Add(i, abcAll[i]);
+
+            foreach (KeyValuePair<int, char> text in alphabet_Mock)
+                Console.Write(text.Key + "-" + text.Value + " ");
+            Console.WriteLine();
+            #endregion
+            string open_text_mock = "";
+            var finalString = "";
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЄЖЗИІЇйКЛМНОПРСТУФХЦЧШЩЬЮЯ1234567890 ?\"'";
+            
+            var random = new Random();
+            #region Pay
+            string open_text = "paymoremoney";
+            #endregion
+           
+
+/*
+            #region Key
+
+            Console.WriteLine("Please, enter key text: ");
+            string key_phrase_mock = Console.ReadLine().ToLower();
+            string key_phrase = "rscrvcfvt";
+            for (int i = 0; i < key_phrase.Length; i++)
+                foreach (KeyValuePair<int, char> text in alphabet)
+                    if (key_phrase[i] == text.Value)
+                        key.Add(text.Key);
+            #endregion
+            #region Key_Mock
+            for (int i = 0; i < key_phrase_mock.Length; i++)
+                foreach (KeyValuePair<int, char> text in alphabet_Mock)
+                    if (key_phrase_mock[i] == text.Value)
+                        key_mock.Add(text.Key);
+            #endregion*/
+            Console.WriteLine("Enter:\n 1 - Encrypt\n 2 - Decrypt");
+            int choice = Convert.ToInt32(Console.ReadLine()); 
+
+            switch (choice)
+            {
+                case 1:
+                    {                
+                        Console.WriteLine("Encrypt HillCipher");
+                        #region Text
+
+                        Console.WriteLine("Please, enter your text:");
+                        open_text_mock = Console.ReadLine();
+                        var stringChars = new char[open_text_mock.Length];
+                        using (StreamWriter sw = new StreamWriter("openText.txt"))
+                        {
+                            sw.WriteLine(open_text_mock);
+                        }
+                        /*for (int i = 0; i < open_text_mock.Length; i++)
+                        {
+                            for (int j = 0; j < 2; j++)
+                                Console.WriteLine(open_text_mock);
+                            Console.WriteLine(open_text_mock);
+                        }*/
+
+                        for (int i = 0; i < open_text.Length; i++)
+                            foreach (KeyValuePair<int, char> text in alphabet)
+                                if (open_text[i] == text.Value)
+                                    plainText.Add(text.Key);
+
+                        for (int i = 0; i < open_text_mock.Length; i++)
+                            foreach (KeyValuePair<int, char> text in alphabet_Mock)
+                                if (open_text_mock[i] == text.Value)
+                                    plainText_Mock.Add(text.Key);
+                        /////foreach (int i in plainText_Mock)
+                        ///// Console.WriteLine(i);
+                        #endregion
+
+                        #region RandomMock
+
+                        for (int i = 0; i < stringChars.Length; i++)
+                        {
+                            stringChars[i] = chars[random.Next(chars.Length)];
+                        }
+                        finalString = new String(stringChars);
+                        using (StreamWriter sw = new StreamWriter("сypher.txt"))
+                        {
+                            sw.WriteLine(finalString);
+                        }
+                        //Console.WriteLine(finalString);
+                        #endregion
+                        #region Key
+
+                        Console.WriteLine("Please, enter key text: ");
+                        string key_phrase_mock = Console.ReadLine().ToLower();
+                        string key_phrase = "rscrvcfvt";
+                        for (int i = 0; i < key_phrase.Length; i++)
+                            foreach (KeyValuePair<int, char> text in alphabet)
+                                if (key_phrase[i] == text.Value)
+                                    key.Add(text.Key);
+                        #endregion
+
+                        #region Key_Mock
+                        for (int i = 0; i < key_phrase_mock.Length; i++)
+                            foreach (KeyValuePair<int, char> text in alphabet_Mock)
+                                if (key_phrase_mock[i] == text.Value)
+                                    key_mock.Add(text.Key);
+                        #endregion
+                        ////
+                        hillCipher.Encrypt_Mock(plainText_Mock, key_mock);
+                        /////
+                        cipher = hillCipher.Encrypt(plainText, key);
+                       
+                        Dictionary<int, char> rezult = new Dictionary<int, char>();
+                        foreach (int i in cipher)
+                            foreach (KeyValuePair<int, char> text in alphabet)
+                                if (i == text.Key)
+                                    encode_rezult += text.Value;
+                       // Console.WriteLine(encode_rezult);                                       
+                        Console.WriteLine(finalString);
+                        Console.ReadLine();
+                        using (StreamWriter sw = new StreamWriter("сypher.txt"))
+                        {
+                            sw.WriteLine(finalString);
+                        }                        
+                        Console.ReadLine();
+                        
+                        break;
+                    }
+                case 2:
+                    {
+                        string finalSrting = "";
+                        List<int> decode = new List<int>();
+                        Console.WriteLine("Decrypt HillCipher");                       
+                        using (StreamReader sr = new StreamReader("сypher.txt"))
+                        {
+                            string char_rezult_decode = encode_rezult;
+                            //string char_rezult_decode = sr.ReadToEnd();
+                            Console.WriteLine("Our cipher from file: ");
+                            finalString=sr.ReadToEnd();
+                            Console.WriteLine(finalString);
+                            
+
+                            for (int i = 0; i < char_rezult_decode.Length; i++)
+                                foreach (KeyValuePair<int, char> text in alphabet)
+                                    if (char_rezult_decode[i] == text.Value)
+                                        decode.Add(text.Key);                                                   
+                        }
+                        if (finalString == null)
+                        {
+                            var cipherD = hillCipher.Decrypt(decode, key);
+
+                            Dictionary<int, char> rezult = new Dictionary<int, char>();
+                            foreach (int i in cipherD)
+                                foreach (KeyValuePair<int, char> text in alphabet)
+                                    if (i == text.Key)
+                                        decode_rezult += text.Value;
+                            Console.WriteLine("Decode rezult:");
+                        }
+                        Console.WriteLine("Text after decoding");
+                        using (StreamReader sw = new StreamReader("openText.txt"))
+                        {
+                            open_text_mock=sw.ReadToEnd();
+                        }
+                        Console.WriteLine(open_text_mock);
+                        Console.ReadLine();                        
+                        break;
+                    }
+                default:
+                    Console.WriteLine("You enter wrong letter");
+                    break;
+            }            
+        }
+    }
+}
